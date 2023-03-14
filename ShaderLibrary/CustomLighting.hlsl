85d054535582b08a08936901662ff16a2b6073ea
AxelSevenS <spaghettiandmeatballs61@gmail.com>
2023-03-14 08:32:02 +0100

    Removed RGB HSV utility function redefinitions.

--- ShaderLibrary/CustomLighting.hlsl
+++ ShaderLibrary/CustomLighting.hlsl
@@ -1,6 +1,7 @@
 #ifndef CEL_LIGHTING_INCLUDED
 #define CEL_LIGHTING_INCLUDED
 
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Color.hlsl"
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/GlobalIllumination.hlsl"
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/RealtimeLights.hlsl"
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/SurfaceData.hlsl"
@@ -9,62 +10,6 @@
 #define _AmbientLight _MainLightColor.rgb*0.35
 
 
-float3 RGBtoHSV(float3 rgb) {
-    float3 hsv;
-    float minVal, maxVal, delta;
-
-    minVal = min(rgb.x, min(rgb.y, rgb.z));
-    maxVal = max(rgb.x, max(rgb.y, rgb.z));
-
-    hsv.z = maxVal;				// v
-    delta = maxVal - minVal;
-
-    if (maxVal != 0)
-        hsv.y = delta / maxVal;		// s
-    else {
-        hsv.y = 0;
-        hsv.x = -1;
-        return hsv;
-    }
-
-    if (rgb.x == maxVal)
-        hsv.x = (rgb.y - rgb.z) / delta;		// between yellow & magenta
-    else if (rgb.y == maxVal)
-        hsv.x = 2 + (rgb.z - rgb.x) / delta;	// between cyan & yellow
-    else
-        hsv.x = 4 + (rgb.x - rgb.y) / delta;	// between magenta & cyan
-
-    hsv.x *= 60;				// degrees
-    if (hsv.x < 0)
-        hsv.x += 360;
-
-    return hsv;
-}
-
-float3 HSVtoRGB(float3 HSV) {
-    float3 RGB = HSV.z;
-
-    float var_h = HSV.x * 6;
-    float var_i = floor(var_h);   // Or ... var_i = floor( var_h )
-    float var_1 = HSV.z * (1.0 - HSV.y);
-    float var_2 = HSV.z * (1.0 - HSV.y * (var_h-var_i));
-    float var_3 = HSV.z * (1.0 - HSV.y * (1-(var_h-var_i)));
-    if (var_i == 0) 
-        RGB = float3(HSV.z, var_3, var_1);
-    else if (var_i == 1) 
-        RGB = float3(var_2, HSV.z, var_1);
-    else if (var_i == 2) 
-        RGB = float3(var_1, HSV.z, var_3);
-    else if (var_i == 3) 
-        RGB = float3(var_1, var_2, HSV.z);
-    else if (var_i == 4) 
-        RGB = float3(var_3, var_1, HSV.z);
-    else                 
-        RGB = float3(HSV.z, var_1, var_2);
-    
-    return (RGB);
-}
-
 float3 ColorSaturation(float3 color, float saturation) {
     if (saturation == 0)
         return color;
@@ -72,12 +17,12 @@ float3 ColorSaturation(float3 color, float saturation) {
     // if (RGBtoSaturation(color) == 0)
     //     return color;
 
-    float3 hsv = RGBtoHSV(color);
+    float3 hsv = RgbToHsv(color);
 
     // hsv.y += saturation;
     // float3 saturatedColor = saturate(HSVtoRGB(hsv));
 
-    float3 saturatedColor = HSVtoRGB(float3(hsv.r, saturation, hsv.b));
+    float3 saturatedColor = HsvToRgb(float3(hsv.r, saturation, hsv.b));
 
     return saturate(saturatedColor);
 }
